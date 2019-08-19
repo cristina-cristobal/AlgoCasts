87e1523a71b01f39c95db817b94ca67b3a40fe78
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-08-19 16:12:16 -0400

    finished palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,15 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// palindrome = (str) => {
+//   x = str.toString().split('')        //converts integers to strings and then splits string into array
+//   let len = x.length                  // saves array length as variable
+//   for (let i=0; i<len/2; i++){        // starts loop at index 0 until midway point, increasing by 1
+//     if (x[i] !== x[len-1-i]){         // in loop, returns false if x[i] !== as mirroring position starting from -1
+//       return false
+//     }
+//   }
+//   return true                         // otherwise returns true
+// }
+
+// palindrome = (str) => {
+//    const reversed = str.split('').reverse().join('')
+//    return str === reversed
+// }
+
 palindrome = (str) => {
-  x = str.toString().split('')        //converts integers to strings and then splits string into array
-  let len = x.length                  // saves array length as variable
-  for (let i=0; i<len/2; i++){        // starts loop at index 0 until midway point, increasing by 1
-    if (x[i] !== x[len-1-i]){         // in loop, returns false if x[i] !== as mirroring position starting from -1
-      return false
-    }
-  }
-  return true                         // otherwise returns true
+  return str.split('').every((char, i) => {
+    return char === str[str.length-1-i]
+  })
 }
 
 module.exports = palindrome;
